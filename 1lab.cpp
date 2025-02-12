832609968371c9a768b5b9ce2e922ad0211319dc
root <root@DESKTOP-I0F9LL8>
2025-02-12 10:11:11 +0300

    infix to postfix try

--- 1lab.cpp
+++ 1lab.cpp
@@ -19,7 +19,7 @@ class Stack
 {
 private:
     char Data[MAX_STACK_SIZE];
-    int TopOfStack = 0;
+    int TopOfStack;
 
 public:
     Stack() : TopOfStack(-1) {}
@@ -33,6 +33,7 @@ public:
     {
         return TopOfStack == MAX_STACK_SIZE - 1;
     }
+
     void push(char item)
     {
         if (FullCheck())
@@ -68,21 +69,21 @@ int prec(char c)
 {
     if (c == '!')
         return 6;
-    else if (c == '<' || c == '<=' || c == '>' || c == '>=' || c == '==' || c == '!=')
+    else if (c == '<' || c == '>')
         return 5;
-    else if (c == '&&')
-        return 4;
-    else if (c == '||')
-        return 3;
+    // else if (c == '&&')
+    //     return 4;
+    // else if (c == '||')
+    //     return 3;
     else if (c == '^')
         return 2;
-    else if (c == '→')
-        return 1;
+    // else if (c == '→')
+    //     return 1;
     else
         return -1;
 }
 
-void infixToPostfix(string s)
+Stack infixToPostfix(string s)
 {
     Stack st;
     Stack result;
@@ -90,24 +91,44 @@ void infixToPostfix(string s)
     for (long unsigned i = 0; i < s.length(); i++)
     {
         char c = s[i];
-        if (c == ' ')
-        {
+
+        if (c == ' '){
             continue;
         }
-        if (isalpha(c))
-        {
-            cout << c << " ";
+
+
+        if (isalpha(c)){
             result.push(c);
         }
-        else
-        {
-            cout << c;
+        else if(c == '('){
+            st.push('(');
         }
+        else if (c == ')'){
+            while(!st.EmptyCheck() && st.peek()!= '('){
+                result.push(st.peek());
+                st.pop();
+            }
+        }
+        else{
+            while(!st.EmptyCheck() && prec(st.peek()) >= prec(c)){
+                result.push(st.peek());
+                st.pop();
+            }
+        st.push(c);
+        }
+
     }
+    while(!result.EmptyCheck())
+    {
+        cout << result.pop() << " ";
+    }
+    
 }
 
 int main()
 {
+    Stack st;
+    Stack result;
     string exp = "a == b";
     infixToPostfix(exp);
     return 0;
