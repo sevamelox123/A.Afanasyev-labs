6ead94e67ec2dc555f3f8dd2441121a6ac9841f7
root <root@DESKTOP-I0F9LL8>
2025-02-13 16:00:10 +0300

    ya lublu enum-_-

--- 1lab.cpp
+++ 1lab.cpp
@@ -9,10 +9,11 @@ using namespace std;
 struct tetoStruct
 {
     bool tetobool;
-    union {
+    union
+    {
         int num;
         char op;
-    }; 
+    };
 };
 
 class Stack
@@ -75,14 +76,14 @@ int prec(char c)
         return 6;
     else if (c == '<' || c == '>')
         return 5;
-    // else if (c == '&&')
-    //     return 4;
-    // else if (c == '||')
-    //     return 3;
+    else if (c == '&&')
+        return 4;
+    else if (c == '||')
+        return 3;
     else if (c == '^')
         return 2;
-    // else if (c == '→')
-    //     return 1;
+    else if (c == '→')
+        return 1;
     else
         return -1;
 }
@@ -98,35 +99,42 @@ Stack infixToPostfix(string s)
     {
         char c = s[i];
 
-        if (isdigit(c)){
-            if (!digitBuffer.has_value()) digitBuffer = 0;
-            digitBuffer = digitBuffer.value() *  10;
+        if (isdigit(c))
+        {
+            if (!digitBuffer.has_value())
+                digitBuffer = 0;
+            digitBuffer = digitBuffer.value() * 10;
             digitBuffer = digitBuffer.value() + (c - '0');
             continue;
         }
-        else if(c == '('){
+        else if (c == '(')
+        {
             tetoStruct teto;
             teto.tetobool = true;
             teto.op = c;
             st.push(teto);
         }
-        else if (c == ')'){
-            if (digitBuffer.has_value()) {
+        else if (c == ')')
+        {
+            if (digitBuffer.has_value())
+            {
                 tetoStruct teto;
                 teto.tetobool = false;
                 teto.num = digitBuffer.value();
                 result.push(teto);
                 digitBuffer.reset();
             }
-            while(!st.EmptyCheck() && st.peek().op != '('){
+            while (!st.EmptyCheck() && st.peek().op != '(')
+            {
                 result.push(st.pop());
-                
             }
             st.pop();
         }
-        else if (c != ' ') {
-            
-            while(!st.EmptyCheck() && prec(st.peek().op) >= prec(c)){
+        else if (c != ' ')
+        {
+
+            while (!st.EmptyCheck() && prec(st.peek().op) >= prec(c))
+            {
                 result.push(st.pop());
             }
             tetoStruct teto;
@@ -134,49 +142,88 @@ Stack infixToPostfix(string s)
             teto.op = c;
             st.push(teto);
         }
-        if (digitBuffer.has_value()) {
+        if (digitBuffer.has_value())
+        {
             tetoStruct teto;
             teto.tetobool = false;
             teto.num = digitBuffer.value();
             result.push(teto);
             digitBuffer.reset();
         }
-        
     }
-    if (digitBuffer.has_value()) {
+    if (digitBuffer.has_value())
+    {
         tetoStruct teto;
         teto.tetobool = false;
         teto.num = digitBuffer.value();
         result.push(teto);
     }
-    
-    while (!st.EmptyCheck()) {
+
+    while (!st.EmptyCheck())
+    {
         result.push(st.pop());
     }
     Stack reversed;
-    while(!result.EmptyCheck())
+    while (!result.EmptyCheck())
     {
         reversed.push(result.pop());
     }
-    while (!reversed.EmptyCheck()) {
-        tetoStruct res = reversed.pop();
-        if (res.tetobool) {
-            std::cout << res.op << " ";
-        }
-        else {
-            std::cout << res.num << " ";
+    // while (!reversed.EmptyCheck())
+    // {
+    //     tetoStruct res = reversed.pop();
+    //     if (res.tetobool)
+    //     {
+    //         std::cout << res.op << " ";
+    //     }
+    //     else
+    //     {
+    //         std::cout << res.num << " ";
+    //     }
+    // }
+    // std::cout << std::endl;
+
+    return reversed;
+}
+bool TetoCalc(Stack tetoStack)
+{
+    Stack st;
+    while(!tetoStack.EmptyCheck())
+    {
+        tetoStruct teto = tetoStack.pop();
+        if(teto.tetobool)
+        {
+            // int right = st.pop().num;
+            if (teto.op == '!') {
+                int right = st.pop().num;
+            }
+            else {
+                int right = st.pop().num;
+                int left = st.pop().num;
+                tetoStruct miku;
+                if (teto.op == '>') {
+                    miku.op = false;
+                    miku.num = static_cast<int>(left > right);
+                }
+                else if (teto.op == '<') {
+                    miku.op = false;
+                    miku.num = static_cast<int>(left < right);
+                }
+                
+                st.push(miku);
+            }
+        } else 
+        {
+            st.push(teto);
         }
     }
-    std::cout << std::endl;
-
-    return result;
+    return static_cast<bool>(st.pop().num);
 }
 
 int main()
 {
     Stack st;
     Stack result;
-    string exp = "100 > (0 < 2)";
-    infixToPostfix(exp);
+    string exp = "100 < (0 < 2)";
+    std::cout << TetoCalc(infixToPostfix(exp)) << std::endl;
     return 0;
 }
